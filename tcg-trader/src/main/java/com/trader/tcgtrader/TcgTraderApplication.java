37cde3737f04c7d119776f579f3372781afb103c
PrinceBurks <133425065+PrinceBurks@users.noreply.github.com>
2024-01-13 18:46:04 -0500

    initial setup of spring project

---
+++ tcg-trader/src/main/java/com/trader/tcgtrader/TcgTraderApplication.java
@@ -0,0 +1,13 @@
+package com.trader.tcgtrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgTraderApplication.class, args);
+	}
+
+}
---
+++ tcg-trader/src/test/java/com/trader/tcgtrader/TcgTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trader.tcgtrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
